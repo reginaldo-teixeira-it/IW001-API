1af5e898d5b0f3583bf8b565a4abdfafd20dbcee
Reginaldo Teixeira <reginaldo.teixeira.ti@gmail.com>
2023-10-29 00:06:12 +0100

    Update data and classe model

---
+++ IW001Solution/CCA.API/Migrations/20231028230032_updateTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CCA.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Loose",
+                table: "CurrentAccountStatement",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Loose",
+                table: "CurrentAccountStatement",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+    }
+}
--- IW001Solution/CCA.API/Model/CurrentAccountStatement.cs
+++ IW001Solution/CCA.API/Model/CurrentAccountStatement.cs
@@ -10,7 +10,7 @@ namespace CCA.API.Model
         public DateTime StartDate { get; set; }
         public float Value { get; set; }
 
-        public string? Loose { get; set; }
+        public bool Loose { get; set; }
         public bool State { get; set; }
 
     }
